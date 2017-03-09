43440b5c84243a5d4e2436aa3bae17d3a6d56570
wasia <wasiabe@gmail.com>
2017-03-09 23:40:40 +0800

    Complete prize number check

--- app/src/main/java/com/example/wasia/prizenumber/PrizeNumber.java
+++ app/src/main/java/com/example/wasia/prizenumber/PrizeNumber.java
@@ -9,14 +9,38 @@ import java.util.List;
 
 public class PrizeNumber {
     public String number;
-    public List<Integer> matchs = new ArrayList<Integer>();
-    public List<Integer> prizes = new ArrayList<Integer>();
+    public ArrayList<Integer> matchs = new ArrayList<Integer>();
+    public ArrayList<Integer> prizes = new ArrayList<Integer>();
 
     public PrizeNumber(){}
 
-    public PrizeNumber(String number, List<Integer> matchs, List<Integer> prizes){
+    public PrizeNumber(String number, ArrayList<Integer> matchs, ArrayList<Integer> prizes){
         this.number = number;
         this.matchs = matchs;
         this.prizes = prizes;
+    }
+
+    public int CheckPrizeNumber(String ReceiptNumber) {
+        int prize = 0;
+        for(int j=0; j<prizes.size(); j++) {
+
+            Long m[] = new Long[matchs.size()];
+            m = matchs.toArray(m);
+
+            Long p[] = new Long[prizes.size()];
+            p = prizes.toArray(p);
+
+            int match =m[j].intValue();
+            int idxBeginP = number.length() - match;
+            String PznbrToMatch = number.substring(idxBeginP, number.length());
+
+            int idxBeginR = ReceiptNumber.length() - match;
+            String ReceiptNbrToMatch = ReceiptNumber.substring(idxBeginR, ReceiptNumber.length());
+
+            if (PznbrToMatch.equals(ReceiptNbrToMatch)) {
+                prize = p[j].intValue();
+            }
+        }
+        return prize;
     }
 }
